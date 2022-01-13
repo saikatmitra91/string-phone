c73322d295664616edfca92e3fe11b4cccb84b36
Saikat  Mitra <saikatmitra@Saikats-MacBook-Air.local>
2022-01-13 17:28:16 +0530

    build fixes

--- components/peer/index.tsx
+++ components/peer/index.tsx
@@ -3,37 +3,47 @@ import {
     useHMSActions,
     useHMSStore
   } from "@100mslive/hms-video-react";
-  import { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
   
-  function Peer({ peer }) {
-    const videoRef = useRef(null);
-    const hmsActions = useHMSActions();
-    const videoTrack = useHMSStore(selectVideoTrackByPeerID(peer.id));
-  
-     useEffect(() => {
-      if (videoRef.current && videoTrack) {
-        if (videoTrack.enabled) {
-          hmsActions.attachVideo(videoTrack.id, videoRef.current);
-        } else {
-          hmsActions.detachVideo(videoTrack.id, videoRef.current);
-        }
+type Peer = {
+  id: string,
+  isLocal: boolean,
+  name: string
+}
+
+type PeerPropsType = {
+  peer: Peer
+}
+
+function Peer({ peer }: PeerPropsType) {
+  const videoRef = useRef(null);
+  const hmsActions = useHMSActions();
+  const videoTrack = useHMSStore(selectVideoTrackByPeerID(peer.id));
+
+    useEffect(() => {
+    if (videoRef.current && videoTrack) {
+      if (videoTrack.enabled) {
+        hmsActions.attachVideo(videoTrack.id, videoRef.current);
+      } else {
+        hmsActions.detachVideo(videoTrack.id, videoRef.current);
       }
-    }, [videoTrack, hmsActions]);
-  
-    return (
-      <div className="peer-container">
-        <video
-          ref={videoRef}
-          className={`peer-video ${peer.isLocal ? "local" : ""}`}
-          autoPlay
-          muted
-          playsInline
-        />
-        <div className="peer-name">
-          {peer.name} {peer.isLocal ? "(You)" : ""}
-        </div>
+    }
+  }, [videoTrack, hmsActions]);
+
+  return (
+    <div className="peer-container">
+      <video
+        ref={videoRef}
+        className={`peer-video ${peer.isLocal ? "local" : ""}`}
+        autoPlay
+        muted
+        playsInline
+      />
+      <div className="peer-name">
+        {peer.name} {peer.isLocal ? "(You)" : ""}
       </div>
-    );
-  }
+    </div>
+  );
+}
   
-  export default Peer;
+export default Peer;
